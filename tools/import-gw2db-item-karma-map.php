ff3bd98755c51b9623fc7019790bc27b663b7f93
Martin Dreher <luma.inc@gmail.com>
2013-02-08 20:12:18 +0100

    added karma for items that have to be bought in bulk

--- tools/import-gw2db-item-karma-map.php
+++ tools/import-gw2db-item-karma-map.php
@@ -27,29 +27,54 @@ $cnt  = count($data);
 $stmt = Propel::getConnection()->prepare("UPDATE gw2db_item_archive 
                                              SET karma_price = :karma_price
                                            WHERE DataId = :data_id");
+                                           
+$bulk = Propel::getConnection()->prepare(
+"UPDATE gw2db_item_archive LEFT JOIN item
+     ON gw2db_item_archive.DataID = item.data_id
+    SET gw2db_item_archive.karma_price = :karma_price, 
+        item.karma_price = :karma_price
+  WHERE gw2db_item_archive.Name IN (:name,:names);");
+  
+// fix for some words where the plural is irregular
+$singular = array(
+    "Tomatoe"   => "Tomato",
+    "Cherrie"   => "Cherry",
+    "Peache"    => "Peach",
+    "Buttermilk"=> "Glass of Buttermilk"
+);
 
 foreach ($data as $i => $row) {
-    if($i % 100 == 0)
-        echo "[{$i} / {$cnt}] \n";
       
     if (strpos($row['Name'], "Recipe: ") !== false) {
         continue;
     }
-	
+    if($i % 100 == 0)
+        echo "[{$i} / {$cnt}]: {$row['Name']} \n";
 	
     if (isset($row['SoldBy']) && isset($row['DataID'])) {
-		$lowestKarma = 0;
+        $lowestKarma = 0;
         foreach($row['SoldBy'] as $r) {    
             if (isset($r['KarmaCost']) && ($r['KarmaCost'] < $lowestKarma || $lowestKarma == 0)) {
                 $lowestKarma = $r['KarmaCost'];
             }
         }
-		
-		if($lowestKarma > 0) { 
-			$stmt->bindValue('karma_price', $lowestKarma);
-			$stmt->bindValue('data_id', $row['DataID']);
-			$stmt->execute();
-		}
+        
+        if($lowestKarma > 0) { 
+            $stmt->bindValue('karma_price', $lowestKarma);
+            $stmt->bindValue('data_id', $row['DataID']);
+            $stmt->execute();
+            
+            if(preg_match("/^(.+?)s? in Bulk$/", $row['Name'], $match)) {
+                $name = $match[1];
+                if(array_key_exists($name, $singular)) $name = $singular[$name];
+                echo "[{$i} / {$cnt}]: {$name} with {$lowestKarma} Karma: ";
+                $bulk->bindValue('name', $name);
+                $bulk->bindValue('names', $name.'[s]');
+                $bulk->bindValue('karma_price', ceil($lowestKarma / 25));
+                $bulk->execute();
+                echo "{$bulk->rowCount()} updated.\n";
+            }
+        }
     }
 }
 
